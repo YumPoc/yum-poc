d98179f3e89ed1da3a779681bf8cfada885b43a6
Guilherme Raulino <guilhermeraulino2704@gmail.com>
2018-09-12 14:53:12 -0300

    Adicionou o javax.servlet
    adicionou a dependência do javax.servlet no projeto

--- yum-poc/src/br/com/poc/yum/teste/ConnectionFactory.java
+++ yum-poc/src/br/com/poc/yum/teste/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class ConnectionFactory{
 		String connectionUrl = "jdbc:sqlserver://guilherme-faria.database.windows.net:1433;Database=yum_poc;user=guigafaria@guilherme-faria;Password=Abcd@123;Encrypt=True;TrustServerCertificate=true;hostNameInCertificate=guilherme-faria.database.windows.net;Connection Timeout=30;";
 		
 		public Connection getConnection() {
@@ -14,6 +14,7 @@ public class ConnectionFactory {
 				e.printStackTrace();
 				return null;
 			}
+			
 			
 		}
 	}
