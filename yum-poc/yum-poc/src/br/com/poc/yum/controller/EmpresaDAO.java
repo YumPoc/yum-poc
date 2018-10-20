00c65d5c986356a69e554a91cd722a2ae2b358b5
Aluno <Aluno@Digital.net>
2018-10-19 21:04:35 -0300

    Adição de modificações na página cadastro e na lógica de conexão java web

---
+++ yum-poc/yum-poc/src/br/com/poc/yum/controller/EmpresaDAO.java
@@ -0,0 +1,22 @@
+package br.com.poc.yum.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/cadastrares")
+public class EmpresaDAO extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String f = req.getParameter("fantasy");
+		PrintWriter w = resp.getWriter();
+		w.println("EAAI MANOW: "+f);
+		System.out.println("foi");
+	}
+}
