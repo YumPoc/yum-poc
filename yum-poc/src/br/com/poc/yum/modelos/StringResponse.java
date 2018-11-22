266da1a15750db291b9459ec7b37600fe99b3d15
Guilherme Raulino <guilhermeraulino2704@gmail.com>
2018-11-22 21:53:43 -0200

    Json funcionando

---
+++ yum-poc/src/br/com/poc/yum/modelos/StringResponse.java
@@ -0,0 +1,14 @@
+package br.com.poc.yum.modelos;
+
+public class StringResponse {
+	
+	private String response;
+	
+	public StringResponse(String s) {
+		this.response = s;
+	}
+	
+	public String getString() {
+		return response;
+	}
+}
